50efe854bfb20752e3cc399fab10835c1e8ef93c
kundan tiwari <kundan.tiwari@bigohtech.com>
2023-02-01 23:50:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    FirstCompComponent
+   ] ,
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
